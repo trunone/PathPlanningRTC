071bb56ad1921952b5a011ffd6931e286f646479
JUSST <jusst@jusst-kobuki.(none)>
2014-10-30 21:14:18 +0900

    Add comments

--- src/PathServiceSVC_impl.cpp
+++ src/PathServiceSVC_impl.cpp
@@ -28,24 +28,29 @@ PathServiceSVC_impl::~PathServiceSVC_impl()
 void PathServiceSVC_impl::get_path(RTC::Path2D_out path, const RTC::Pose2D& position, const RTC::Pose2D& target)
 {
     CSerializablePtr serializable_ptr;
+    // Convert the simple map from string format to serializable object
     StringToObject(rtc_ptr_->get_map(), serializable_ptr);
 
+    // The path
     std::deque<TPoint2D> deque_path;
 
     COccupancyGridMap2D grid_map;
+    // Convert serializable simple map object to grid map
     grid_map.loadFromSimpleMap(*CSimpleMapPtr(serializable_ptr));
 
     CPathPlanningCircularRobot path_planning;
-    path_planning.robotRadius = 0.30f;
+    path_planning.robotRadius = 0.15f;
 
     bool not_found;
+    // Find the path
     path_planning.computePath(grid_map,
             CPose2D(position.position.x, position.position.y, position.heading),
             CPose2D(target.position.x, target.position.y, target.heading),
             deque_path, not_found, 1000.0f);
 
     path = new RTC::Path2D();
-    if(!not_found) {
+    if(!not_found) { // if found any path
+        // Copy path from deque_path to path
         path->waypoints.length(deque_path.size());
         int i = 0;
         std::deque<TPoint2D>::const_iterator it = deque_path.begin();
@@ -54,7 +59,7 @@ void PathServiceSVC_impl::get_path(RTC::Path2D_out path, const RTC::Pose2D& posi
             path->waypoints[i].target.position.y = it->y;
             path->waypoints[i].target.heading = 0.0f;
         }
-    } else {
+    } else { // if didn't find the path
         path->waypoints.length(0);
         cout<<"Not Found"<<endl;
     }
